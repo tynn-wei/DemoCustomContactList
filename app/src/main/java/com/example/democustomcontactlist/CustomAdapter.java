4f8b50f745dbff5f2077b0002a2ab7836c3d726b
20019616 <20019616@rp.edu.sg>
2021-07-29 18:14:04 +0800

    Initial commit

---
+++ app/src/main/java/com/example/democustomcontactlist/CustomAdapter.java
@@ -0,0 +1,55 @@
+package com.example.democustomcontactlist;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class CustomAdapter extends ArrayAdapter {
+
+    Context parent_context;
+    int layout_id;
+    ArrayList<Contact> contactList;
+
+    public CustomAdapter (Context context, int resource, ArrayList<Contact> objects) {
+        super(context, resource, objects);
+        parent_context = context;
+        layout_id = resource;
+        contactList = objects;
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater inflater = (LayoutInflater) parent_context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View rowView = inflater.inflate(layout_id, parent, false);
+
+        TextView tvName = rowView.findViewById(R.id.tvName);
+        TextView tvCode = rowView.findViewById(R.id.tvCountryCode);
+        TextView tvNum = rowView.findViewById(R.id.tvPhoneNum);
+        ImageView ivGender = rowView.findViewById(R.id.imageViewGender);
+
+        Contact currentItem = contactList.get(position);
+        tvName.setText(currentItem.getName());
+        tvCode.setText("+" + currentItem.getCountryCode());
+        tvNum.setText(currentItem.getPhoneNum() + "");
+
+        if(currentItem.getGender()=='F') {
+            ivGender.setImageResource(R.drawable.female);
+        } else {
+            ivGender.setImageResource(R.drawable.male);
+        }
+
+        return rowView;
+    }
+}
